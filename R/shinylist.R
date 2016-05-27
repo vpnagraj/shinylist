2b94f18470722e3668d2e48da97808567ae335b8
VP Nagraj <vpnagraj@virginia.edu>
2016-05-27 16:02:54 -0400

    updated documentation for functions

--- R/shinylist.R
+++ R/shinylist.R
@@ -1,6 +1,6 @@
 #' HTML lists with R
 #'
-#' Create an HTML list from R input. Vector input will be parsed by element and output as either ordered or unordered list item(s). Can be used inside of Shiny app with \code{\link{listOuput}} and \code{\link{renderList}}.
+#'  Create an HTML list from R input. Vector input will be parsed by element and output as either ordered or unordered list item(s). Can be used inside of Shiny app with \code{\link{listOuput}} and \code{\link{renderList}}.
 #'
 #' @param dat the vector of data to be output as HTML list
 #' @param oderered indicator of whether or not list should be ordered (i.e. numbered) or unordered
@@ -32,14 +32,12 @@ shinylist <- function(dat, ordered = FALSE, width = NULL, height = NULL) {
     )
 }
 
-#' @name listOutput for Shiny
 #'
 #' @export
 listOutput <- function(outputId, width = '100%', height = '400px'){
     htmlwidgets::shinyWidgetOutput(outputId, 'shinylist', width, height, package = 'shinylist')
 }
 
-#' @name renderList for Shiny
 #'
 #' @export
 renderList <- function(expr, env = parent.frame(), quoted = FALSE) {
